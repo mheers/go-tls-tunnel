29057c65977dd408c73b26a5688a48bab3f354b8
Marcel Heers <info@heers.it>
2020-03-20 08:19:24 +0100

    handles closing of connections

--- tunnel.go
+++ tunnel.go
@@ -4,36 +4,43 @@ package tunnel
 
 import (
 	"crypto/tls"
-	"log"
+	"errors"
 	"net"
 	"strconv"
+
+	"github.com/sirupsen/logrus"
 )
 
-type tunnel struct {
+// Tunnel describes a tunnel
+type Tunnel struct {
 	localAddr          string
 	remoteAddr         string
 	remoteHost         string
 	maxConn            int16
 	insecureSkipVerify bool
+	listener           *net.TCPListener
+	stop               bool
 }
 
-func NewTunnel(remoteHost string, remotePort uint16, localHost string, localPort uint16, maxConn int16, insecureSkipVerify bool) tunnel {
-	t := &tunnel{
+// NewTunnel creates a new tunnel
+func NewTunnel(remoteHost string, remotePort uint16, localHost string, localPort uint16, maxConn int16, insecureSkipVerify bool) *Tunnel {
+	t := &Tunnel{
 		localAddr:          localHost + ":" + strconv.Itoa(int(localPort)),   //  "127.0.0.1:5588",
 		remoteAddr:         remoteHost + ":" + strconv.Itoa(int(remotePort)), // "example.com:4444",
 		remoteHost:         remoteHost,
 		maxConn:            maxConn,
 		insecureSkipVerify: insecureSkipVerify,
+		stop:               false,
 	}
-	return *t
+	return t
 }
 
-func (t tunnel) proxyConn(conn net.Conn) {
+func (t *Tunnel) proxyConn(conn net.Conn) {
 	defer conn.Close()
 
 	rAddr, err := net.ResolveTCPAddr("tcp", t.remoteAddr)
 	if err != nil {
-		log.Print(err)
+		logrus.Print(err)
 	}
 
 	conf := &tls.Config{
@@ -44,18 +51,18 @@ func (t tunnel) proxyConn(conn net.Conn) {
 	//rConn, err := net.DialTCP("tcp", nil, rAddr)
 	rConn, err := tls.Dial("tcp", rAddr.String(), conf)
 	if err != nil {
-		log.Print(err)
+		logrus.Print(err)
 		return
 	}
 	defer rConn.Close()
 
-	//	log.Printf("remoteAddr connected: %v\n", rAddr.String())
+	//	logrus.Printf("remoteAddr connected: %v\n", rAddr.String())
 
 	t.pipe(conn, rConn)
-	//	log.Printf("proxyConn end: %v -> %v\n", conn.RemoteAddr(), rConn.RemoteAddr())
+	//	logrus.Printf("proxyConn end: %v -> %v\n", conn.RemoteAddr(), rConn.RemoteAddr())
 }
 
-func (t tunnel) chanFromConn(conn net.Conn) chan []byte {
+func (t *Tunnel) chanFromConn(conn net.Conn) chan []byte {
 	c := make(chan []byte)
 
 	go func() {
@@ -79,7 +86,7 @@ func (t tunnel) chanFromConn(conn net.Conn) chan []byte {
 	return c
 }
 
-func (t tunnel) pipe(conn1 net.Conn, conn2 net.Conn) {
+func (t *Tunnel) pipe(conn1 net.Conn, conn2 net.Conn) {
 	chan1 := t.chanFromConn(conn1)
 	chan2 := t.chanFromConn(conn2)
 
@@ -88,74 +95,87 @@ func (t tunnel) pipe(conn1 net.Conn, conn2 net.Conn) {
 		case b1 := <-chan1:
 			if b1 == nil {
 				return
-			} else {
-				conn2.Write(b1)
 			}
+			conn2.Write(b1)
+
 		case b2 := <-chan2:
 			if b2 == nil {
 				return
-			} else {
-				conn1.Write(b2)
 			}
+			conn1.Write(b2)
 		}
 	}
 }
 
-func (t tunnel) StartTunnel() {
-	log.SetFlags(log.Lshortfile)
+// Close closes the connection
+func (t *Tunnel) Close() error {
+	if t.listener != nil {
+		t.stop = true
+		return t.listener.Close()
+	}
+	return errors.New("listener is nil")
+}
 
-	log.Printf("Listening: %v -> %v\n\n", t.localAddr, t.remoteAddr)
+// StartTunnel starts the tunnel
+func (t *Tunnel) StartTunnel() error {
+	logrus.Printf("Listening: %v -> %v\n\n", t.localAddr, t.remoteAddr)
 
 	addr, err := net.ResolveTCPAddr("tcp", t.localAddr)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	listener, err := net.ListenTCP("tcp", addr)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	t.listener = listener
+	// defer listener.Close()
 
 	var i int16 = 0
 	count := make(chan int16, 1)
-	for {
-		log.Printf("wait accepted...\n")
-		conn, err := listener.AcceptTCP()
-		if err != nil {
-			log.Print(err)
+	go func() {
+		for !t.stop {
+			logrus.Printf("wait accepted...\n")
+			conn, err := listener.AcceptTCP()
+			if err != nil {
+				logrus.Print(err)
+			}
+			// t.conn = conn
+			// defer conn.Close()
+			//go proxyConn(conn);
+
+			//logrus.Printf("accepted: %v\n", conn.RemoteAddr())
+			select {
+			case t := <-count:
+				i += t
+				//				print("received ", i, "\n")
+
+			default:
+
+			}
+			if i < t.maxConn {
+				i++
+				logrus.Printf("[%v/%v]accepted: %v\n", i, t.maxConn, conn.RemoteAddr())
+
+				// Create a new goroutine which will call the connection handler and  then free up the space.
+				go func(connection net.Conn) {
+					t.proxyConn(connection)
+					//				logrus.Printf("[%v/%v]Closed connection from %s\r\n", i, maxConn, connection.RemoteAddr())
+					logrus.Printf("Closed connection from %s\r\n", connection.RemoteAddr())
+					select {
+					case t := <-count:
+						count <- t - 1
+
+					default:
+						count <- -1
+					}
+				}(conn)
+			} else {
+				conn.Close()
+				logrus.Printf("closed: %v\n", i)
+			}
 		}
-		//go proxyConn(conn);
-
-		//log.Printf("accepted: %v\n", conn.RemoteAddr())
-		select {
-		case t := <-count:
-			i += t
-			//				print("received ", i, "\n")
-
-		default:
-
-		}
-		if i < t.maxConn {
-			i++
-			log.Printf("[%v/%v]accepted: %v\n", i, t.maxConn, conn.RemoteAddr())
-
-			// Create a new goroutine which will call the connection handler and  then free up the space.
-			go func(connection net.Conn) {
-				t.proxyConn(connection)
-				//				log.Printf("[%v/%v]Closed connection from %s\r\n", i, maxConn, connection.RemoteAddr())
-				log.Printf("Closed connection from %s\r\n", connection.RemoteAddr())
-				select {
-				case t := <-count:
-					count <- t - 1
-
-				default:
-					count <- -1
-				}
-			}(conn)
-		} else {
-			conn.Close()
-			//			log.Printf("closed: %v\n", i)
-		}
-	}
-
+	}()
+	return nil
 }
